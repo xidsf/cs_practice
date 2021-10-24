3b116f65bcb8a181dc55f7ffe13a842c12ef5618
xidsf <gooderic816053@gmail.com>
2021-10-24 15:35:13 +0900

    async

---
+++ async/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Toast { }
+class Juice { }
+class Bacon { }
+class Egg { }
+class Coffee { }
+
+
+namespace async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
