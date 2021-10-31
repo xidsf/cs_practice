677d659facc0be91ebc74b72bec16433d043e5ae
xidsf <gooderic816053@gmail.com>
2021-10-31 12:25:09 +0900

    temp

---
+++ 37Reference01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player
+{
+    public int HP = 100;
+    public int AT = 10;
+}
+
+
+
+
+namespace _37Reference01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player NewPlayer = new Player();
+            NewPlayer.AT = 50;
+            NewPlayer = new Player();
+
+        }
+    }
+}
